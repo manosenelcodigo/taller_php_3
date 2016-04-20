b50171cab598515d863232b4f31be186bfc51e81
#manosenelcódigo <peligrosamente@gmail.com>
2016-04-19 23:24:48 -0300

    archivo.php

---
+++ archivo.php
@@ -0,0 +1,23 @@
+<?php
+//persistencia
+
+/*
+    - Temporal: se almacena información en tiempo de ejecución de la aplicación
+        
+        + variables
+        + sessiones
+        + cookies 
+    
+    - Remota.... consultas remotas que generalmente se trabajan con APIs SOAP o RestFull
+    - Permanente.... bases de datos, txt, xml
+*/
+
+/*
+aplicación 
+
+    - conjuntos de datos
+    - desplegados en una interfaz, gráfica
+    - que es utilizada por un grupo de usuarios, con distintos perfiles de acceso a las funcionalidades
+*/
+
+?>
