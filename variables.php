c2e6e853701b866a7dbbdb96c425a6895e138437
#manosenelcódigo <peligrosamente@gmail.com>
2016-04-19 23:25:10 -0300

    variables.php

---
+++ variables.php
@@ -0,0 +1,30 @@
+<?php
+// RAM
+/*
+variable
+este espacio en memoria se reserva, solo en tiempo de ejecución de la aplicación
+
+en el pc del cliente
+en el pc del servidor apache
+
+un millón de usuarios conectados de manera simultánea
+
+- por cada usuario el servidor crea una instancia temporal de tiempo de ejecución del script
+*/  
+$mivariable='23';
+$mi_variable_bonita;
+$miVariableBonita;
+
+/*
+Tipos de datos
+    números : enteros,decimales
+    textos, cadenas, string
+    fechas
+    boleanos
+    bytes
+*/
+
+
+
+
+
